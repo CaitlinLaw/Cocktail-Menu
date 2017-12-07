c676d5db934ae9e9268a11e45baf60f10535f1be
BriCurry <32935961+BriCurry@users.noreply.github.com>
2017-12-07 17:49:28 +0000

    Final for publishing

--- javacocktail.js
+++ javacocktail.js
@@ -18,6 +18,11 @@ function top_tip2(){
 		alert("FACT: Looks like someone is hungover!") + '.\n';
 }
 
+function top_tip3(){
+
+		alert("This cocktail is Cait's favourite!") + '.\n';
+}
+
 /*$(document).ready(function() {
 $('#show').hide();
 function show_method(){
